a3b739c6c989da748e4f17380097fbc929613cde
Binay shrestha <binayshrestha798@gmail.com>
2021-10-07 07:51:22 +0545

    background and paper with heading added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
 import './App.css';
+import { Paper } from '@mui/material';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontSize: '1rem',
+  },
+});
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <ThemeProvider theme={theme}>
+      <div className='App'>
+        <Paper
+          sx={{ padding: '3rem', maxWidth: '70rem', width: '100%' }}
+          elevation={6}
+        >
+          <div className='head'>
+            <h2 className='head__primary'>Image Converter</h2>
+            <p className='head__para'>Convert your image files online & free</p>
+          </div>
+        </Paper>
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
